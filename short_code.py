cbe723ed04c7d5fbee172534f8f39752df710f40
Andre-Amaro <123480192+Andre-Amaro@users.noreply.github.com>
2023-06-16 14:57:03 -0300

    Create short_code.py

---
+++ short_code.py
@@ -0,0 +1,14 @@
+"""
+Writing short code
+Define a function named convert that takes a list of numbers as its only parameter and returns a list of each number converted to a string.
+
+For example, the call convert([1, 2, 3]) should return ["1", "2", "3"].
+
+What makes this tricky is that your function body must only contain a single line of code.
+
+"""
+
+def convert(text):
+    return [str(num) for num in text]
+
+# convert([1,2,3,4])  <- deixar essa linha no codigo da erro no desafio :/
